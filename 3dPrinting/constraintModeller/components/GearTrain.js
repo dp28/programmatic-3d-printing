57b73f764c944b06dbdfb9726064da51a3a85b4c
Daniel Patterson <dp28@st-andrews.ac.uk>
2014-01-20 16:42:28 +0000

    Modifying GearTrain so that only valid Gears can be added - those that have/can be modified to have the correct circular pitch

--- 3dPrinting/constraintModeller/components/GearTrain.js
+++ 3dPrinting/constraintModeller/components/GearTrain.js
@@ -8,11 +8,74 @@ var Component = require('../components/Component.js').Component
 var ConstrainableValue = require('../constraints/ConstrainableValue.js').ConstrainableValue
 var Gear = require('../components/Gear.js').Gear
 var Utilities = require('../Utilities.js')
+var util = require('util')
 
 module.exports.GearTrain = GearTrain
 
 Utilities.inheritPrototype(GearTrain, Component)
 
-function GearTrain() {
+function checkIfCreationIsLegal(circPitch) {
+	if (circPitch == undefined) throw new Error("No circular pitch specified")
+	if (circPitch <= 0) throw new Error("Invalid circular pitch specified")
+}
+
+/*
+ * Creates a GearTrain to collect Gears together. Requires a circular pitch to 
+ * be specified so that all Gears in this GearTrain can have the same tooth 
+ * size. This is necessary for the gears to mesh.
+ */ 
+function GearTrain(circPitch) {
+	checkIfCreationIsLegal(circPitch)
 	Component.call(this)
+	var gears = []
+	var circularPitch = circPitch
+
+	this.addGear = function(gear) {
+		this.checkIfGearCanBeAdded(gear)
+		this.changeGearToHaveSameCircularPitch(gear)
+		gears.push(gear)
+	} 
+
+	this.checkIfGearCanBeAdded = function(gear) {
+		if (gear.getNumberOfTeeth().isNotSet() 
+			  && gear.getPitchCircleRadius().isNotSet()) {
+			throw new Error("Number of teeth or pitch circle radius not set")
+		}
+		try {
+			if (gear.getCircularPitch() == circularPitch) 
+				return
+		}
+		catch(err) {
+			// error should be thrown as circular pitch should not be calculable
+			return
+		}
+		throw new Error("Circular pitch of Gear does not match GearTrain")
+	}
+
+	this.changeGearToHaveSameCircularPitch = function(gear) {
+		if (gear.getNumberOfTeeth().isNotSet()) {
+			var numTeeth = calculateNumberOfTeethFromPitchCircleRadius(gear)
+			gear.setNumberOfTeeth(numTeeth)
+		}
+		else if (gear.getPitchCircleRadius().isNotSet()) {
+			var radius = calculatePitchCircleRadiusFromNumberOfTeeth(gear)
+			gear.setPitchCircleRadius(radius)
+		}
+	}
+
+	var calculateNumberOfTeethFromPitchCircleRadius = function(gear) {
+		return Math.PI * 2 * gear.getPitchCircleRadius().getValue() / circularPitch
+	}
+
+	var calculatePitchCircleRadiusFromNumberOfTeeth = function(gear) {
+		return circularPitch * gear.getNumberOfTeeth().getValue() / (2 * Math.PI)
+	}
+
+	this.getGears = function() {
+		return gears
+	}
+
+	this.getCircularPitch = function() {
+		return circularPitch
+	}
 }
--- 3dPrinting/constraintModeller/tests/GearTrainTest.js
+++ 3dPrinting/constraintModeller/tests/GearTrainTest.js
@@ -8,17 +8,136 @@ var Component = require('../components/Component.js').Component
 var Gear = require('../components/Gear.js').Gear 
 var GearTrain = require('../components/GearTrain.js').GearTrain
 var ComponentTest = require('../tests/ComponentTest.js')
+var GearTest = require('../tests/GearTest.js')
 
 describe('GearTrain', function() {
 	var train 
 
 	beforeEach(function() {
-		train = new GearTrain() 
+		train = new GearTrain(8) 
 	})
 
 	it('should behave like a Component', function() {
 		ComponentTest.shouldBehaveLikeComponent(train)
 	})
 
-	
+	describe('construction', function() {
+		it('should not be possible to create a GearTrain without specifying a ' 
+			 + 'circular pitch', function(){
+			(function() {
+				new GearTrain()
+			}).should.throw("No circular pitch specified")
+		})
+
+		it('should not be possible to create a GearTrain by specifying a ' 
+			 + 'circular pitch of 0 or below', function(){
+			(function() {
+				new GearTrain(0)
+			}).should.throw("Invalid circular pitch specified")
+		})
+
+		it('should be possible to create a GearTrain with a circular pitch greater '
+			 + 'than 0', function() {
+			(function() {
+				new GearTrain(10)
+			}).should.not.throw()
+		})
+	})
+
+	describe('#addGear', function() {
+		var gear
+		var numberOfTeeth = 15
+		var pitchCircleRadius = 20
+
+		// Formula for circular pitch, see
+		// http://www.cs.cmu.edu/~rapidproto/mechanisms/chpt7.html#HDR116A 
+		var circularPitch = Math.PI * pitchCircleRadius * 2 / numberOfTeeth
+
+		beforeEach(function() {
+			gear = new Gear()
+			train = new GearTrain(circularPitch)
+		})
+
+		describe('Adding a gear with neither its number of teeth or pitch circle '
+			       + 'radius set', function() {
+			it ('should not be possible', function() {
+				var error = "Number of teeth or pitch circle radius not set"
+				try {
+					train.addGear(gear).should.throw(error)
+				}
+				catch(err) {
+					err.message.should.eql(error)
+				}
+			})
+		})
+
+		describe('Adding a gear with its number of teeth set but pitch circle radius'
+		         + ' not yet set', function() {
+			beforeEach(function() {
+				gear.setNumberOfTeeth(numberOfTeeth)
+				train.addGear(gear)
+			})
+
+			it('should set the pitch circle radius of the Gear to the value '
+				 + 'corresponding to the circular pitch of the GearTrain', function() {
+				gear.getPitchCircleRadius().getValue().should.be.approximately(pitchCircleRadius, 0.001)
+			})			
+		})
+
+		describe('Adding a gear with its pitch circle radius set but its number of '
+			       + 'teeth not set', function() {
+			beforeEach(function() {
+				gear.setPitchCircleRadius(pitchCircleRadius)
+				train.addGear(gear)
+			})
+
+			it('should set the pitch circle radius of the Gear to the value '
+				 + 'corresponding to the circular pitch of the GearTrain', function() {
+				gear.getNumberOfTeeth().getValue().should.be.approximately(numberOfTeeth, 0.001)
+			})
+			
+		})
+
+		describe('Adding a gear with both its number of teeth and pitch circle ' 
+			       + 'radius set', function() {
+			beforeEach(function() {
+				gear.setNumberOfTeeth(numberOfTeeth)
+				gear.setPitchCircleRadius(pitchCircleRadius)
+			})
+
+			it('should not be possible if the circular pitch of the Gear is different '
+				 + 'from that of the GearTrain', function() {
+				train = new GearTrain(circularPitch + 1)
+				var error = "Circular pitch of Gear does not match GearTrain";
+				(function() {
+					train.addGear(gear)
+				}
+				).should.throw(error)
+			})
+
+			it('should be possible if their circular pitches are the same',
+			   function() {
+				train = new GearTrain(circularPitch)
+				train.getGears().length.should.equal(0)			
+				train.addGear(gear)
+				train.getGears().length.should.equal(1)			
+				train.getGears()[0].should.be.an.instanceof(Gear)	
+			})
+		})
+
+		describe('Adding a gear with the number of teeth specified but the pitch radius unspecified', function() {
+			beforeEach(function() {
+				gear = new Gear()
+				gear.setNumberOfTeeth()
+			})
+		})
+	})
+
+	describe('#getCircularPitch', function() {
+		it('should be the same as the value used to create it', function() {
+			var circularPitch = 10
+			train = new GearTrain(circularPitch)
+			train.getCircularPitch().should.equal(circularPitch)
+		})
+	})
 })
