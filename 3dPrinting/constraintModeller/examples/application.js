2b559c6fa3c8921f9ca7f78efc9aff30bfd1fdba
Daniel Patterson <dp28@st-andrews.ac.uk>
2014-01-22 17:51:37 +0000

    Adding an example

---
+++ 3dPrinting/constraintModeller/examples/application.js
@@ -0,0 +1,21 @@
+/*
+ * A sample application that creates a simple GearTrain
+ */
+var GearTrain = require('../components/GearTrain.js').GearTrain
+var Gear = require('../components/Gear.js').Gear
+var SpecificationWriter = require('../interface/SpecificationWriter.js').SpecificationWriter
+
+var gearTrain = new GearTrain(10)
+var firstGear = gearTrain.createGear(10)
+var secondGear = gearTrain.createGear(20)
+var thirdGear = gearTrain.createGear(5)
+var fourthGear = gearTrain.createGear(15)
+var fifthGear = gearTrain.createGear(25)
+var writer = new SpecificationWriter()
+firstGear.meshOnLeftOf(secondGear)
+thirdGear.meshAtBackOf(secondGear)
+fourthGear.meshAtFrontOf(secondGear)
+fifthGear.meshOnRightOf(secondGear)
+secondGear.getCentre().setAt(0, 0, 0)
+writer.addComponent(gearTrain)
+writer.writeSpecificationToFile()
