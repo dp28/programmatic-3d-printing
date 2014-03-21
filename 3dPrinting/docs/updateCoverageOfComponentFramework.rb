d7206db2aa38213f28baee41f91bcc18e3b203b9
Daniel Patterson <dp28@st-andrews.ac.uk>
2014-03-21 11:19:01 +0000

    Correcting coverage-updating script to properly run interface tests

--- 3dPrinting/docs/updateCoverageOfComponentFramework.rb
+++ 3dPrinting/docs/updateCoverageOfComponentFramework.rb
@@ -5,41 +5,52 @@ FILES_NOT_TO_INSTRUMENT = ['node_modules', 'tests', 'Utilities.js', 'Configurati
 CONSTRAINT_MODELLER_NAME = 'constraintModeller'
 CONSTRAINT_MODELLER_LOCATION = "../#{CONSTRAINT_MODELLER_NAME}"
 TEST_DIR_NAME = "tests"
-COVERAGE_CODE_DIR = '.instrumented_code'
 COVERAGE_DOCUMENT = 'componentFrameworkCoverage.html'
 
 def instrumentConstraintDirectory(directory)
-	`jscoverage #{CONSTRAINT_MODELLER_LOCATION}/#{directory} #{COVERAGE_CODE_DIR}/#{directory}`
+	`jscoverage #{CONSTRAINT_MODELLER_LOCATION}/#{directory} #{directory}`
 end
 
 def instrumentConstraintModellerCode()
-	`mkdir #{COVERAGE_CODE_DIR}`
-	codeDirectories = Dir["#{CONSTRAINT_MODELLER_LOCATION}/*/"].map { |name|
-		File.basename(name)
-	}
-	codeDirectories -= FILES_NOT_TO_INSTRUMENT
-	codeDirectories -= DIRECTORIES_TO_IGNORE
+	codeDirectories = findCodeDirectories()
 	codeDirectories.each { |directory| 
 		instrumentConstraintDirectory(directory)
 	}
 end
 
+def findCodeDirectories() 
+	codeDirectories = Dir["#{CONSTRAINT_MODELLER_LOCATION}/*/"].map { |name|
+		File.basename(name)
+	}
+	codeDirectories -= FILES_NOT_TO_INSTRUMENT
+	codeDirectories -= DIRECTORIES_TO_IGNORE
+	return codeDirectories
+end
+
 def copyTestsToInstrumentedCode() 
 	FILES_NOT_TO_INSTRUMENT.each { |file| 
-		`cp -r #{CONSTRAINT_MODELLER_LOCATION}/#{file} #{COVERAGE_CODE_DIR}/#{file}`
+		print `cp -r #{CONSTRAINT_MODELLER_LOCATION}/#{file} #{file}`
 	}
 end
 
 def generateCoverageDocument() 
-	`mocha #{COVERAGE_CODE_DIR}/#{TEST_DIR_NAME}/*Test.js -R html-cov >> #{COVERAGE_DOCUMENT}`
+	print `mocha #{TEST_DIR_NAME}/*Test.js -R html-cov >> #{COVERAGE_DOCUMENT}`
 end
 
 def removeInstrumentedCode() 
-	`rm -r #{COVERAGE_CODE_DIR}`
+	removeDirectories(findCodeDirectories())
+	removeDirectories(FILES_NOT_TO_INSTRUMENT)
+end
+
+def removeDirectories(directories)
+	directories.each {|directory|
+		`rm -r #{directory}`
+	}
+
 end
 
 def removeOldCoverageDocument() 
-	`rm #{COVERAGE_DOCUMENT}`
+	print `rm #{COVERAGE_DOCUMENT}`
 end
 
 removeOldCoverageDocument()
