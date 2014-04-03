fe23fb93b884fe6291953a2db6675317ffbe65c9
Daniel Patterson <dp28@st-andrews.ac.uk>
2014-04-03 18:27:49 +0100

    Adding Licence and header to every file

--- 3dPrinting/constraintModeller/interface/MainFileWriter.js
+++ 3dPrinting/constraintModeller/interface/MainFileWriter.js
@@ -1,4 +1,24 @@
 /*
+ * This file is part of programmatic-3d-printing, a programmatic 3d printer 
+ * language, Copyright (c) 2014, Daniel Patterson <dan24patt@gmail.com>.
+ * All rights reserved.
+ * 
+ * programmatic-3d-printing is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * programmatic-3d-printing is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ */
+
+/*
  * author: Daniel Patterson
  *
  * Generates the main file (the point of entry) for the 3D Drawer, including 
